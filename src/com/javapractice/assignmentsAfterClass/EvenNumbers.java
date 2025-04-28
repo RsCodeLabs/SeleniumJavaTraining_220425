94b9ab6ba0ae93dcbccb5bf82e5ff198206edd2e
PSupriya123 <paramallasupriya2022@gmail.com>
2025-04-28 11:54:51 -0700

    Assignment-28April

---
+++ src/com/javapractice/assignmentsAfterClass/EvenNumbers.java
@@ -0,0 +1,19 @@
+package com.javapractice.assignmentsAfterClass;
+
+import java.lang.reflect.GenericDeclaration;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.println("Even numbers from 1 to " +n +":");
+        for (int i = 1; i<=n; i++)
+        {
+            if(i%2 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
---
+++ src/com/javapractice/assignmentsAfterClass/SumOf100Nos.java
@@ -0,0 +1,11 @@
+package com.javapractice.assignmentsAfterClass;
+
+public class SumOf100Nos {
+    public static void main(String[] args) {
+        int sum = 0, n = 100;
+        for (int i = 0; i <=n ; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of numbers from 1 till 100 :" +sum);
+    }
+}
