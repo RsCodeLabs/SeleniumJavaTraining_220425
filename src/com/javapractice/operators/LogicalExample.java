fd465dba6bfdbe753ebebcf057909d29d81dc2be
yrt225 <yrt225@gmail.com>
2025-04-28 22:58:27 +0530

    Day 5 - Loops, Conditional, Jump statements & Operators

---
+++ src/com/javapractice/operators/LogicalExample.java
@@ -0,0 +1,10 @@
+package com.javapractice.operators;
+
+public class LogicalExample {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println(a && b); // false
+        System.out.println(a || b); // true
+        System.out.println(!a);     // false
+    }
+}
---
+++ src/com/javapractice/operators/RelationalExample.java
@@ -0,0 +1,9 @@
+package com.javapractice.operators;
+
+public class RelationalExample {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println(a > b);   // true
+        System.out.println(a == b);  // false
+    }
+}
