16284f29a58cffcb3197df2dbb3f9984351c9384
Deepaly4irkal <deepaly4irkal@gmail.com>
2025-04-29 13:11:58 -0400

    Updated school name

---
+++ src/com/javapractice/operators/assignment.java
@@ -0,0 +1,32 @@
+package com.javapractice.operators;
+
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class assignment {
+    public static void main(String[] args) {
+//      #  1. Print Even Numbers using Modulus Operator
+//       # ( Clue : n%2==0 is even, n%2==1 is odd
+//       # 2.Find sum of 100  numbers (Clue : loop i till 100 using for loop , make sum += i and print sum)
+
+        String num = System.input("Enter a number: ");
+        Scanner sc = new Scanner(in)
+        if(n%2==0){
+            out.println("Even number");
+        elif(n%2==1);
+            out.println("Odd number");
+        }
+
+        int sum = 0;
+        for(int i=0; i>100; i++){
+            sum = sum + i;
+            out.println("i value"+ (sum+i) );
+
+        }
+
+
+    }
+
+}
