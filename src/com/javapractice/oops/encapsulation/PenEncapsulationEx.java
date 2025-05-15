539438951a7e29e757eaf697b02bfc32b6a69cbc
yrt225 <yrt225@gmail.com>
2025-05-16 00:15:40 +0530

    Added Encapsulation & abstraction classes

---
+++ src/com/javapractice/oops/encapsulation/PenEncapsulationEx.java
@@ -0,0 +1,42 @@
+package com.javapractice.oops.encapsulation;
+
+public class PenEncapsulationEx {
+
+    //Declaration of variables
+     private String name = "Reynolds";
+     private String color = "Blue";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+/*//created Methods
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public String getColor(){
+            return color;
+        }
+
+        public void setColor(String color){
+            this.color = color;
+        }
+*/
+}
---
+++ src/com/javapractice/oops/encapsulation/PenEncapsulationUsage.java
@@ -0,0 +1,14 @@
+package com.javapractice.oops.encapsulation;
+
+public class PenEncapsulationUsage {
+    public static void main(String[] args) {
+
+         PenEncapsulationEx penEncapsulationEx = new PenEncapsulationEx();
+        System.out.println("name of the pen"+penEncapsulationEx.getName());
+        penEncapsulationEx.setColor("Red");
+        System.out.println("color of the pen"+penEncapsulationEx.getColor());
+
+
+
+    }
+}
