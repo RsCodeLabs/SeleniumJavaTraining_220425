0a42c476a1cc77ab937669bee903a1583b9479f4
PSupriya123 <paramallasupriya2022@gmail.com>
2025-05-17 08:04:26 -0700

    Assignment 13-May16

---
+++ src/com/javapractice/supriyaAssignments/java/Bike.java
@@ -0,0 +1,8 @@
+package com.javapractice.supriyaAssignments.java;
+
+public class Bike extends Vehicle {//Create a class Bike that extends Vehicle and adds a method doWheelie().
+
+    public void doWheelie() {
+        System.out.println("Do Wheelie - Bike class");
+    }
+}
