43e228aad84cc80aea4f2ea0c6a123279137fcfb
yrt225 <yrt225@gmail.com>
2025-05-22 00:54:17 +0530

    Collections - Arrays Vs arryalist

---
+++ src/com/javapractice/collections/ArrayListExample.java
@@ -0,0 +1,81 @@
+package com.javapractice.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        // Create an ArrayList
+        ArrayList<String > fruits = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Date");
+
+
+        for(String fruit: fruits){
+            System.out.println(fruit);
+        }
+
+        System.out.println("Fruits in the list:"+fruits);
+
+        //adding/inserting element at specific index
+        fruits.add(2, "Mango");
+
+        System.out.println("After adding specific index:"+fruits);
+
+        // Get an element from the ArrayList
+        String fruitAtIndex4 = fruits.get(4);
+        System.out.println("Fruit at index 4: " + fruitAtIndex4);
+
+        //Update an element in the ArrayList
+        fruits.set(1, "Blueberry");
+        System.out.println("Fruit at index 4: " + fruits.get(1));
+        System.out.println("Fruits in the list:"+fruits);
+
+        // Remove an element from the ArrayList
+        fruits.remove("Cherry");
+        System.out.println("Fruits in the list:"+fruits);
+        fruits.remove(1);
+        System.out.println("Fruits in the list:"+fruits);
+
+        //Contains
+        boolean containsBlueBerry = fruits.contains("Blueberry");
+        System.out.println("Contains Blueberry: " + containsBlueBerry);
+
+
+        //size()
+        int size = fruits.size();
+        System.out.println("Size of the list: " + size);
+
+        //clear
+        fruits.clear();
+        System.out.println("Fruits in the list:"+fruits);
+
+        //isEmpty
+        boolean isEmpty = fruits.isEmpty();
+        System.out.println("Is the list empty? " + isEmpty);
+
+        //indexOf
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Date");
+        fruits.add("Banana");
+
+
+
+        int indexOfBanana = fruits.indexOf("Banana");
+        System.out.println("Index of Banana: " + indexOfBanana);
+
+        //Last index
+        int lastIndexOfBanana = fruits.lastIndexOf("Banana");
+        System.out.println("Last index of Banana: " + lastIndexOfBanana);
+
+
+
+    }
+}
---
+++ src/com/javapractice/collections/ArrayVsArrayList.java
@@ -0,0 +1,34 @@
+package com.javapractice.collections;
+
+import java.util.HashSet;
+
+public class ArrayVsArrayList {
+    public static void main(String[] args) {
+
+        String[] fruitsArray = new String[3];
+        fruitsArray[0] = "Apple";
+        fruitsArray[1] = "Banana";
+        fruitsArray[2] = "Cherry";
+        System.out.println("Array:"+ fruitsArray);
+        // fruitsArray[3] = "Date"; // This will cause an ArrayIndexOutOfBoundsException
+
+       // fruitsArray[0] = 10; // This will cause a compile-time error, as the array is of type String
+
+//Arraylist
+        HashSet<Object> fruitsList = new HashSet<>();
+        fruitsList.add("Apple");
+        fruitsList.add("Banana");
+        fruitsList.add("Cherry");
+        fruitsList.add("Cherry");
+        fruitsList.add("Date"); // This is allowed, as ArrayList can grow dynamically
+    fruitsList.add(10); // This is allowed, as ArrayList can hold different types of objects
+        fruitsList.add(10.5); // This is allowed, as ArrayList can hold different types of objects
+        fruitsList.add(true); // This is allowed, as ArrayList can hold different types of objects
+
+
+        System.out.println("ArrayList:"+ fruitsList);
+
+        // Print the elements of the array
+
+    }
+}
