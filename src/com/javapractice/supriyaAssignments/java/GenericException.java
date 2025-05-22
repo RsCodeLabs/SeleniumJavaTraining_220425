22ab5be940a6293b8f105448fad1169d73488f59
PSupriya123 <paramallasupriya2022@gmail.com>
2025-05-22 13:17:37 -0700

    Assignment22- Need Clarification on usage of Generic exception

---
+++ src/com/javapractice/supriyaAssignments/java/GenericException.java
@@ -0,0 +1,7 @@
+package com.javapractice.supriyaAssignments.java;
+
+public class GenericException extends Exception {
+    public GenericException(String message) {
+        super(message);
+    }
+}
